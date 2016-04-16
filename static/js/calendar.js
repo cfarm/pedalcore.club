c6dbaf449125422374c44214f9070e725b94d30e
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-04-16 16:40:23 -0400

    js to get events from google calendar, jquery, compiled js

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,102 +1,50 @@
-var CLIENT_ID = '';
+var $ = require('jquery');
+var keys = require('./keys.js');
 
-var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
-
-/**
- * Check if current user has authorized this application.
- */
-function checkAuth() {
-  gapi.auth.authorize(
-    {
-      'client_id': CLIENT_ID,
-      'scope': SCOPES.join(' '),
-      'immediate': true
-    }, handleAuthResult);
+function Calendar(calendarId, key) {
+  this.calendarId = calendarId;
+  this.key = key;
 }
 
-/**
- * Handle response from authorization server.
- *
- * @param {Object} authResult Authorization result.
- */
-function handleAuthResult(authResult) {
-  var authorizeDiv = document.getElementById('authorize-div');
-  if (authResult && !authResult.error) {
-    // Hide auth UI, then load client library.
-    authorizeDiv.style.display = 'none';
-    loadCalendarApi();
-  } else {
-    // Show auth UI, allowing the user to initiate authorization by
-    // clicking authorize button.
-    authorizeDiv.style.display = 'inline';
+Calendar.prototype.getEvents = function() {
+  var REQUEST_URL = 
+    'https://www.googleapis.com/calendar/v3/calendars/' 
+     + this.calendarId
+     + '/events?maxResults=1&orderBy=startTime&singleEvents=true&key='
+     + this.key;
+
+  var jqxhr = $.ajax({
+    url: REQUEST_URL
+    })
+    .done(function(data) {
+      // success
+      console.log(data)
+      var summaryText = Calendar.prototype.getEventSummary(data.items);
+      var $nextRide = $('#next-ride');
+      Calendar.prototype.displaySummary(summaryText, $nextRide);
+    })
+    .fail(function() {
+      // error
+    })
+    .always(function() {
+      // complete
+    });
+}
+
+Calendar.prototype.getEventSummary = function(eventList) {
+  var summary = '';
+  if (eventList.length) {
+    summary = eventList[0].summary;
   }
+  console.log(summary)
+  return summary;
 }
 
-/**
- * Initiate auth flow in response to user clicking authorize button.
- *
- * @param {Event} event Button click event.
- */
-function handleAuthClick(event) {
-  gapi.auth.authorize(
-    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-    handleAuthResult);
-  return false;
+Calendar.prototype.displaySummary = function(summary, $container) {
+  $container.text(summary);
+  console.log($container)
 }
 
-/**
- * Load Google Calendar client library. List upcoming events
- * once client library is loaded.
- */
-function loadCalendarApi() {
-  gapi.client.load('calendar', 'v3', listUpcomingEvents);
-}
-
-/**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
- */
-function listUpcomingEvents() {
-  var request = gapi.client.calendar.events.list({
-    'calendarId': 'primary',
-    'timeMin': (new Date()).toISOString(),
-    'showDeleted': false,
-    'singleEvents': true,
-    'maxResults': 10,
-    'orderBy': 'startTime'
-  });
-
-  request.execute(function(resp) {
-    var events = resp.items;
-    appendPre('Upcoming events:');
-
-    console.log(resp)
-
-    if (events.length > 0) {
-      for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        var when = event.start.dateTime;
-        if (!when) {
-          when = event.start.date;
-        }
-        appendPre(event.summary + ' (' + when + ')')
-      }
-    } else {
-      appendPre('No upcoming events found.');
-    }
-
-  });
-}
-
-/**
- * Append a pre element to the body containing the given message
- * as its text node.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-  var pre = document.getElementById('output');
-  var textContent = document.createTextNode(message + '\n');
-  pre.appendChild(textContent);
-}
+var KEY = keys.KEYS.CALENDAR;
+var bikeCalendar = new Calendar('pedalcore@gmail.com', KEY);
+bikeCalendar.getEvents();
