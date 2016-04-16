7a9dd9180c4ba50f59d2c5e363f24ffc64a34cd0
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-04-16 18:27:55 -0400

    why am i sorting dates this is dumb

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -10,7 +10,7 @@ Calendar.prototype.getEvents = function() {
   var REQUEST_URL = 
     'https://www.googleapis.com/calendar/v3/calendars/' 
      + this.calendarId
-     + '/events?maxResults=1&orderBy=startTime&singleEvents=true&key='
+     + '/events?orderBy=startTime&singleEvents=true&key='
      + this.key;
 
   var jqxhr = $.ajax({
@@ -19,7 +19,7 @@ Calendar.prototype.getEvents = function() {
     .done(function(data) {
       // success
       console.log(data)
-      var summaryText = Calendar.prototype.getEventSummary(data.items);
+      var summaryText = Calendar.prototype.getNextEvent(data.items);
       var $nextRide = $('#next-ride');
       Calendar.prototype.displaySummary(summaryText, $nextRide);
     })
@@ -29,6 +29,78 @@ Calendar.prototype.getEvents = function() {
     .always(function() {
       // complete
     });
+}
+
+// date or dateTime is RFC3339 format
+Calendar.prototype.getFutureDates = function(eventDate) {
+
+  var today = new Date();
+  var currentYear = today.getFullYear();
+ // add 1 so we can compare to google calendar format which starts at 1
+  var currentMonth = today.getMonth() + 1;
+  var eventString = eventDate.toString();
+  var month = eventString.slice(5, 7);
+  var year = eventString.slice(0,4);
+  var dateList = [];
+
+  function dateIsInTheFuture() {
+    isFutureDate = true;
+    dateList.push(eventDate);
+  }
+
+  if (year > currentYear) {
+    dateIsInTheFuture();
+  } else if (month == currentMonth) {
+    var currentDate = today.getDate();
+    var date = eventString.slice(8, 10);
+    if (date >= currentDate) {
+      dateIsInTheFuture();
+    }
+  } else if (month > currentMonth) {
+    dateIsInTheFuture()
+  } else {
+    isFutureDate = false;
+  }
+
+  var sortedDateList = function() {
+    var numberList = [];
+    for (var i = 0; i < dateList.length; i++) {
+      var item = dateList[i];
+      if (typeof item === String) {
+        item = new Number(item);
+      }
+      numberList.push(item);
+    }
+    numberList.sort();
+    return numberList;
+  }
+
+  return {
+    isFutureDate: isFutureDate,
+    sortedDateList: sortedDateList()
+  }
+
+}
+
+Calendar.prototype.getNextEvent = function(eventList) {
+  var nextEvent = null;
+  var upcomingEvents = [];
+  if (eventList.length) {
+    $(eventList).each(function() {
+      // if upcoming
+      var eventDate = this.start.dateTime || this.start.date;
+      if (eventDate.length && Calendar.prototype.getFutureDates(eventDate)) {
+        // if this.start.dateTime.length && this.start.dateTime > today
+        console.log(Calendar.prototype.getFutureDates(eventDate));
+        upcomingEvents.push(this);
+      }
+    });
+  }
+  console.log(upcomingEvents);
+  // sort upcomingEvents by date
+  nextEvent = upcomingEvents[0];
+  console.log(nextEvent);
+  return nextEvent.summary;
 }
 
 Calendar.prototype.getEventSummary = function(eventList) {
